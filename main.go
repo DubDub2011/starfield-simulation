ce739ae80a956a5a56468fb05610ae9f09f64c24
Owen Watkins <owen.watkins10@outlook.com>
2025-07-28 08:29:01 +0100

    add 2d library

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 1280, 720
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	ebiten.SetWindowSize(1280, 720)
+
+	game := &Game{}
+	ebiten.RunGame(game)
 }
